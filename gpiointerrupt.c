f5fba5308554766cf6fa7af1d125daa49a2a4178
Joshua Beaton <142354375+jobeaton@users.noreply.github.com>
2024-08-25 15:10:43 -0400

    Add files via upload

--- gpiointerrupt.c
+++ gpiointerrupt.c
@@ -35,58 +35,161 @@
  */
 #include <stdint.h>
 #include <stddef.h>
-#include <stdio.h>
 
 /* Driver Header files */
 #include <ti/drivers/GPIO.h>
-#include <ti/drivers/I2C.h>
-#include <ti/drivers/Timer.h>
-#include <ti/drivers/UART2.h>
 
 /* Driver configuration */
 #include "ti_drivers_config.h"
+#include <ti/drivers/Timer.h>
 
-#define DISPLAY(x) UART2_write(uart, &output, x, NULL);
+#define DOT_DURATION      500000   // dot
+#define DASH_DURATION     1500000  // dash
+#define CHAR_SPACE        1500000  // character space
+#define WORD_SPACE        3500000  // word space
 
-// I2C Global Variables
-static const struct {
-    uint8_t address;
-    uint8_t resultReg;
-    char *id;
-} sensors[3] = {
-    { 0x48, 0x0000, "11X" },
-    { 0x49, 0x0000, "116" },
-    { 0x41, 0x0001, "006" }
-};
-uint8_t             txBuffer[1];
-uint8_t             rxBuffer[2];
-I2C_Transaction     i2cTransaction;
+typedef enum {
+    STATE_SOS, //Sos
+    STATE_OK, //OK
+    STATE_IDLE //Idle
+} MessageState;
 
-// UART Global Variables
-char                output[64];
-int                 bytesToSend;
+typedef enum {
+    LED_RED,   // dot
+    LED_GREEN, // dash
+    LED_OFF    // space
+} LEDState;
 
-// Driver Handles - Global variables
-I2C_Handle      i2c;
-UART2_Handle     uart;
-Timer_Handle    timer0;
-
-volatile unsigned char TimerFlag = 0;
-void timerCallback(Timer_Handle myHandle, int_fast16_t status)
-{
-    TimerFlag = 1;
+MessageState currentState = STATE_SOS;
+uint8_t morseIndex = 0; //Index for node
+uint32_t blinkTiming = 0;
+uint32_t delayCounter = 0;
+const uint8_t morseCodeSOS[] = { 1, 1, 1, 0, 2, 2, 2, 0, 1, 1, 1 }; // SOS
+const uint8_t morseCodeOK[] = { 2, 1, 2, 0, 1 }; // OK
+uint8_t buttonPressed = 0;
+LEDState LED_STATE = LED_OFF;
+// sets the states of the LEDs
+void setMorseLEDs() {
+    switch(LED_STATE) {
+        case LED_RED:
+            GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_ON);
+            GPIO_write(CONFIG_GPIO_LED_1, CONFIG_GPIO_LED_OFF);
+            printf("RED\n"); // Debug print
+            break;
+        case LED_GREEN:
+            GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_OFF);
+            GPIO_write(CONFIG_GPIO_LED_1, CONFIG_GPIO_LED_ON);
+            printf("GREEN\n"); // Debug print
+            break;
+        case LED_OFF:
+            GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_OFF);
+            GPIO_write(CONFIG_GPIO_LED_1, CONFIG_GPIO_LED_OFF);
+            printf("OFF\n"); // Debug print
+            break;
+        default:
+            break;
+    }
 }
 
+void timerCallback(Timer_Handle myHandle, int_fast16_t status)
+{
+
+    // Check if delay
+    if (delayCounter < blinkTiming) {
+        delayCounter++;
+        return;
+    }
+
+    // Resets the counter
+    delayCounter = 0;
+
+    if (currentState == STATE_SOS) {
+        if (morseIndex < sizeof(morseCodeSOS) / sizeof(morseCodeSOS[0])) {
+            printf("Processing SOS, Morse Index: %d\n", morseIndex);
+            if (morseCodeSOS[morseIndex] == 1) { // Dot
+                LED_STATE = LED_RED;
+                blinkTiming = DOT_DURATION; // time for dot
+            } else if (morseCodeSOS[morseIndex] == 2) { // Dash
+                LED_STATE = LED_GREEN;
+                blinkTiming = DASH_DURATION; // time for dash
+            } else { // Space
+                LED_STATE = LED_OFF;
+                blinkTiming = CHAR_SPACE; // time for space
+                morseIndex++; // next symbol
+            }
+            // Update LEDs before moving to the next symbol
+            setMorseLEDs(); // Set LED states for the current LED_STATE
+            morseIndex++;
+        } else {
+            // reset
+            morseIndex = 0;
+            LED_STATE = LED_OFF; // lights off
+            setMorseLEDs(); // off
+            blinkTiming = WORD_SPACE; // timefor word space
+        }
+    } else if (currentState == STATE_OK) {
+        if (morseIndex < sizeof(morseCodeOK) / sizeof(morseCodeOK[0])) {
+            printf("Processing OK, Morse Index: %d\n", morseIndex);
+            if (morseCodeOK[morseIndex] == 1) { // Dot
+                LED_STATE = LED_RED;
+                blinkTiming = DOT_DURATION; // time for dot
+            } else if (morseCodeOK[morseIndex] == 2) { // Dash
+                LED_STATE = LED_GREEN;
+                blinkTiming = DASH_DURATION; // time for dash
+            } else { // Space
+                LED_STATE = LED_OFF;
+                blinkTiming = CHAR_SPACE; // time for space
+                morseIndex++; // next symbol
+            }
+            setMorseLEDs();
+            morseIndex++;
+        } else {
+            // reset
+            morseIndex = 0;
+            LED_STATE = LED_OFF;
+            setMorseLEDs(); // Turn off
+            blinkTiming = WORD_SPACE; // time for word space
+        }
+    }
+}
+void initTimer(void)
+{
+Timer_Handle timer0;
+Timer_Params params;
+Timer_init();
+Timer_Params_init(&params);
+params.period = 500000;
+params.periodUnits = Timer_PERIOD_US;
+params.timerMode = Timer_CONTINUOUS_CALLBACK;
+ params.timerCallback = timerCallback;
+
+timer0 = Timer_open(CONFIG_TIMER_0, &params);
+if (timer0 == NULL) {
+/* Failed to initialized timer */
+while (1) {}
+}
+if (Timer_start(timer0) == Timer_STATUS_ERROR) {
+/* Failed to start timer */
+while (1) {}
+}
+}
 /*
  *  ======== gpioButtonFxn0 ========
  *  Callback function for the GPIO interrupt on CONFIG_GPIO_BUTTON_0.
  *
  *  Note: GPIO interrupts are cleared prior to invoking callbacks.
  */
-volatile unsigned char Button0Flag = 0;
 void gpioButtonFxn0(uint_least8_t index)
 {
-    Button0Flag = 1;
+    if (delayCounter >= blinkTiming) {
+        // Toggle the message state
+        if (currentState == STATE_SOS) {
+            currentState = STATE_OK;
+        } else {
+            currentState = STATE_SOS;
+        }
+        buttonPressed = 1; // Set flag
+    }
 }
 
 /*
@@ -96,184 +199,27 @@ void gpioButtonFxn0(uint_least8_t index)
  *
  *  Note: GPIO interrupts are cleared prior to invoking callbacks.
  */
-volatile unsigned char Button1Flag = 0;
 void gpioButtonFxn1(uint_least8_t index)
 {
-    Button1Flag = 1;
+    /* Toggle an LED */
+    GPIO_toggle(CONFIG_GPIO_LED_1);
 }
-
-void initUART(void)
-{
-    UART2_Params uartParams;
-    size_t bytesRead;
-    size_t bytesWritten = 0;
-    uint32_t status     = UART2_STATUS_SUCCESS;
-
-    /* Create a UART where the default read and write mode is BLOCKING */
-    UART2_Params_init(&uartParams);
-    uartParams.baudRate = 115200;
-
-    uart = UART2_open(CONFIG_UART2_0, &uartParams);
-
-    if (uart == NULL)
-    {
-        /* UART2_open() failed */
-        while (1) {}
-    }
-}
-
-// Make sure you call initUART() before calling this function.
-void initI2C(void)
-{
-    int8_t              i, found;
-    I2C_Params          i2cParams;
-
-    DISPLAY(snprintf(output, 64, "Initializing I2C Driver - "))
-
-    // Init the driver
-    I2C_init();
-
-    // Configure the driver
-    I2C_Params_init(&i2cParams);
-    i2cParams.bitRate = I2C_400kHz;
-
-    // Open the driver
-    i2c = I2C_open(CONFIG_I2C_0, &i2cParams);
-    if (i2c == NULL)
-    {
-        DISPLAY(snprintf(output, 64, "Failed\n\r"))
-        while (1);
-    }
-
-    DISPLAY(snprintf(output, 32, "Passed\n\r"))
-
-    // Boards were shipped with different sensors.
-    // Welcome to the world of embedded systems.
-    // Try to determine which sensor we have.
-    // Scan through the possible sensor addresses
-
-    /* Common I2C transaction setup */
-    i2cTransaction.writeBuf   = txBuffer;
-    i2cTransaction.writeCount = 1;
-    i2cTransaction.readBuf    = rxBuffer;
-    i2cTransaction.readCount  = 0;
-
-    found = false;
-    for (i=0; i<3; ++i)
-    {
-        i2cTransaction.targetAddress = sensors[i].address;
-        txBuffer[0] = sensors[i].resultReg;
-
-        DISPLAY(snprintf(output, 64, "Is this %s? ", sensors[i].id))
-        if (I2C_transfer(i2c, &i2cTransaction))
-        {
-            DISPLAY(snprintf(output, 64, "Found\n\r"))
-            found = true;
-            break;
-        }
-        DISPLAY(snprintf(output, 64, "No\n\r"))
-    }
-
-    if(found)
-    {
-        DISPLAY(snprintf(output, 64, "Detected TMP%s I2C address: %x\n\r", sensors[i].id, i2cTransaction.targetAddress))
-    }
-    else
-    {
-        DISPLAY(snprintf(output, 64, "Temperature sensor not found, contact professor\n\r"))
-    }
-}
-
-
-void initTimer(void)
-{
-    Timer_Params    params;
-
-    // Init the driver
-    Timer_init();
-
-    // Configure the driver
-    Timer_Params_init(&params);
-    params.period = 100000;
-    params.periodUnits = Timer_PERIOD_US;
-    params.timerMode = Timer_CONTINUOUS_CALLBACK;
-    params.timerCallback = timerCallback;
-
-    // Open the driver
-    timer0 = Timer_open(CONFIG_TIMER_0, &params);
-
-    if (timer0 == NULL) {
-        /* Failed to initialized timer */
-        while (1) {}
-    }
-
-    if (Timer_start(timer0) == Timer_STATUS_ERROR) {
-        /* Failed to start timer */
-        while (1) {}
-    }
-
-    DISPLAY( snprintf(output, 64, "Timer Configured\n\r"))
-}
-
-int16_t readTemp(void)
-{
-    int     j;
-    int16_t temperature = 0;
-
-    i2cTransaction.readCount  = 2;
-    if (I2C_transfer(i2c, &i2cTransaction))
-    {
-        /*
-         * Extract degrees C from the received data;
-         * see TMP sensor datasheet
-         */
-        temperature = (rxBuffer[0] << 8) | (rxBuffer[1]);
-        temperature *= 0.0078125;
-
-        /*
-         * If the MSB is set '1', then we have a 2's complement
-         * negative value which needs to be sign extended
-         */
-        if (rxBuffer[0] & 0x80)
-        {
-            temperature |= 0xF000;
-        }
-    }
-    else
-    {
-        DISPLAY(snprintf(output, 64, "Error reading temperature sensor (%d)\n\r",i2cTransaction.status))
-        DISPLAY(snprintf(output, 64, "Please power cycle your board by unplugging USB and plugging back in.\n\r"))
-    }
-
-    return temperature;
- }
 
 /*
  *  ======== mainThread ========
  */
 void *mainThread(void *arg0)
 {
-    int16_t     temperature = 0;
-    int16_t     setpoint = 20;  // start at 20c
-    uint16_t    timer200ms = 0;
-    uint16_t    timer500ms = 0;
-    uint32_t    seconds = 0;
-    uint8_t     heat = 0;
-    uint32_t    timer = 0;  // Add timer
-  //  size_t bytesWritten = 0;
-
     /* Call driver init functions */
     GPIO_init();
 
-#ifdef CONFIG_GPIO_TMP_EN
-    GPIO_setConfig(CONFIG_GPIO_TMP_EN, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_HIGH);
-    /* Allow the sensor to power on */
-    sleep(1);
-#endif
-
     /* Configure the LED and button pins */
     GPIO_setConfig(CONFIG_GPIO_LED_0, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
+    GPIO_setConfig(CONFIG_GPIO_LED_1, GPIO_CFG_OUT_STD | GPIO_CFG_OUT_LOW);
     GPIO_setConfig(CONFIG_GPIO_BUTTON_0, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING);
+
+    /* Turn on user LED */
+    GPIO_write(CONFIG_GPIO_LED_0, CONFIG_GPIO_LED_ON);
 
     /* Install Button callback */
     GPIO_setCallback(CONFIG_GPIO_BUTTON_0, gpioButtonFxn0);
@@ -281,67 +227,17 @@ void *mainThread(void *arg0)
     /* Enable interrupts */
     GPIO_enableInt(CONFIG_GPIO_BUTTON_0);
 
-    /*
-     *  If more than one input pin is available for your device, interrupts
-     *  will be enabled on CONFIG_GPIO_BUTTON1.
-     */
-    if (CONFIG_GPIO_BUTTON_0 != CONFIG_GPIO_BUTTON_1) {
-        /* Configure BUTTON1 pin */
+
+    if (CONFIG_GPIO_BUTTON_0 != CONFIG_GPIO_BUTTON_1)
+    {
+
         GPIO_setConfig(CONFIG_GPIO_BUTTON_1, GPIO_CFG_IN_PU | GPIO_CFG_IN_INT_FALLING);
 
-        /* Install Button callback */
         GPIO_setCallback(CONFIG_GPIO_BUTTON_1, gpioButtonFxn1);
         GPIO_enableInt(CONFIG_GPIO_BUTTON_1);
     }
 
-
-    initUART(); // The UART must be initialized before calling initI2C()
-    DISPLAY( snprintf(output, 64, "UART + GPIO + Timer +I2C + Interrupts by Eric Gregori\n\r"))
-    DISPLAY( snprintf(output, 64, "GPIO + Interrupts configured\n\r"))
-    initI2C();
-    initTimer();
-
-    // Loop Forever
-    // The student should add flags (similiar to the timer flag) to the button handlers.
-    // Timer interrupt set to 100ms
-    DISPLAY( snprintf(output, 64, "Starting Task Scheduler\n\r"))
-    while (1)
-    {
-        // Every 200ms check the button flags
-        if (timer % 2 == 0) {
-            // Adjust set-point based on button flags
-            if (Button0Flag) {
-                setpoint++;  // Increment set-point
-                Button0Flag = 0;  // Reset flag
-            }
-            if (Button1Flag) {
-                setpoint--;  // Decrement set-point
-                Button1Flag = 0;  // Reset flag
-            }
-        }
-
-        // Every 500ms read the temperature and update the LED
-        if (timer % 5 == 0) {
-            temperature = readTemp();  // Read the temperature
-        }
-
-        // Update the LED and report every second
-        if (timer % 10 == 0) {
-            heat = (temperature < setpoint) ? 1 : 0;  // Control LED/Heater
-            GPIO_write(CONFIG_GPIO_LED_0, heat);      // Turn on/off LED
-
-            // Output to UART
-            DISPLAY(snprintf(output, 64, "<%02d,%02d,%d,%04d>\n\r", temperature, setpoint, heat, seconds));
-
-            seconds++;  // Increment seconds counter
-        }
-        // Refer to ZyBooks - "Converting different-period tasks to C
-
-        // Wait for the next timer interrupt
-        while (!TimerFlag) {} // Wait for timer period
-        TimerFlag = 0;  // Lower flag raised by timer
-        timer++;  // Increment timer (100ms intervals)
-    }
+    initTimer(); // Initialize the timer
 
     return (NULL);
 }
